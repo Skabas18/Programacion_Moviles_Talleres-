ac429529519f2021b58e786d5b37f8f9bc47a795
unknown <sebastian.cabal@arknova.co>
2022-09-04 16:45:39 -0500

    creacion y navegacion de 2 vistas

--- Taller5/Login/app/src/main/java/com/taller5/login/MainActivity.java
+++ Taller5/Login/app/src/main/java/com/taller5/login/MainActivity.java
@@ -2,13 +2,38 @@ package com.taller5.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button  enviar;
+    TextView forgot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        forgot = (TextView) findViewById(R.id.forgot);
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LostAccount.class);
+                startActivity(i);
+            }
+        });
+
+        enviar = (Button) findViewById(R.id.submit);
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Menu.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
